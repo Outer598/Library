f0b02ab4c774e75a357753f98288fc63fa3e1092
FA <fisayoaasa@gmail.com>
2025-07-11 15:50:46 +0100

    Updated queries

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 This is a project that offers people the chance to write their book or upload their comics for free.
 
 It makes use of Vite, React, Express, and Tailwind CSS.
+
+## TODO
+
+- [ ] Modify insert query to when acces control is handles on the system.
--- server/config/queries/Insertbooks.js
+++ server/config/queries/Insertbooks.js
@@ -2,7 +2,8 @@ import db from "../db/dbSetup.js";
 
 export const insertBook = async (name, cover, author, synopsis) =>{
     try{
-        await db.query("INSERT INTO books (name, cover, author, synopsis) VALUES ($1, $2, $3, $4)", [name, cover, author, synopsis]);
+        let authorID = await db.query('SELECT id FROM users WHERE name = $1', [author]); // come and modify when access control is handled
+        await db.query("INSERT INTO books (name, cover, author, synopsis) VALUES ($1, $2, $3, $4)", [name, cover, authorID.rows[0], synopsis]);
         console.log('done')
     } catch (error) {
         console.log(error.message);
